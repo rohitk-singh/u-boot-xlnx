d31a6a0cdbcd59f311974993f5299a7dcb834bf8
Rohit Kumar Singh <rohit91.2008@gmail.com>
2016-08-11 17:41:32 +0530

    Add Zynq Styx support to u-boot

---
+++ include/configs/zynq_styx.h
@@ -0,0 +1,11 @@
+#ifndef __CONFIG_ZYNQ_STYX_H
+#define __CONFIG_ZYNQ_STYX_H
+
+/* Define STYX PS Clock Frequency to 40MHz */
+#define CONFIG_ZYNQ_PS_CLK_FREQ	40000000UL
+#define CONFIG_ZYNQ_I2C0
+#define CONFIG_ZYNQ_EEPROM
+
+#include <configs/zynq-common.h>
+
+#endif /* __CONFIG_ZYNQ_STYX_H */
